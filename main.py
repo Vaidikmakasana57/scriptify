dd11fe5c39b66d44ede11f79025f054c760e1ada
Vaidikmakasana57 <2301031000037@silveroakuni.ac.in>
2025-07-26 09:29:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Scriptify AI", layout="centered")
 
-col1, col2 = st.columns([1, 6])
-
-with col1:
-    st.image("logo.png", width=60)
-
-with col2:
-    st.markdown("## Welcome to Scriptify AI!")
+st.title("Welcome to Scriptify AI!")
 
 st.write("""
 Navigate using the sidebar:
